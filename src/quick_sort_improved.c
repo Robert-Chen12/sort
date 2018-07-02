a2ddfa9ff67eb8a858d30bda3a951fcfe5a13650
Robert-Chen12 <40195326+Robert-Chen12@users.noreply.github.com>
2018-07-03 07:40:05 +0900

    Update quick_sort_improved.c

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -9,6 +9,33 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+  int i,j,l,r,pivot,z;
+    if(n<=1) return;
+    pivot = A[0];
+    l=0;
+    r=n;
+  for(i = j =1; i<r; i++){
+    if(A[i] < pivot){
+      z = A[l];
+      A[l] = A[i];
+      A[i] = z;
+      l++;
+      j++;
+    }
+   else if(A[i] > pivot){
+        z = A[r-1];
+        A[r-1] = A[i];
+        A[i] = z;
+        r--;
+        i--;
+    }
+    else if(A[i]==pivot){
+    	i++;
+    }
+  }
+
+  quick_sort(A,l);
+  quick_sort(A+r,n-r);
 }
 
 int main(){
